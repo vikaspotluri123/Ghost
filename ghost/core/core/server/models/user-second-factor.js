2792be4aa54ee4738a0b2d0390bd0b770a458ba3
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-04-02 22:19:28 -0700

    added users second factors model

---
+++ ghost/core/core/server/models/user-second-factor.js
@@ -0,0 +1,34 @@
+const errors = require('@tryghost/errors');
+const {getMfaService} = require('../services/auth/multifactor.js');
+const ghostBookshelf = require('./base');
+
+const UsersSecondFactor = ghostBookshelf.Model.extend({
+    tableName: 'users_second_factors',
+
+    User() {
+        return this.belongsTo('User');
+    }
+}, {
+    create(type, userId) {
+        const mfaService = getMfaService();
+        try {
+            return mfaService.defaults(type, userId);
+        } catch (err) {
+            if (mfaService.isPublicError(err)) {
+                throw new errors.ValidationError({message: err.message, err});
+            }
+
+            throw err;
+        }
+    }
+});
+
+const UsersSecondFactors = ghostBookshelf.Collection.extend({
+    model: UsersSecondFactor
+});
+
+module.exports = {
+    UsersSecondFactor: ghostBookshelf.model('UsersSecondFactor', UsersSecondFactor),
+    UsersSecondFactors: ghostBookshelf.collection('UsersSecondFactors', UsersSecondFactors)
+};
+
