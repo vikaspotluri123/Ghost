cee91d4492658482c32f683ea5fa88f91867bef1
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-04-30 20:05:31 -0700

    Added factor type to factor list

--- ghost/admin/app/models/users-second-factor.js
+++ ghost/admin/app/models/users-second-factor.js
@@ -5,7 +5,20 @@ export default Model.extend({
     description: attr('string'),
     status: attr('string'),
     type: attr('string'),
-    context: attr('string', {
+    context: attr({
         defaultValue: '' // Context is only included for shared secrets that have not yet been revealed
-    })
+    }),
+
+    get niceType() {
+        switch (this.get('type')) {
+        case 'otp':
+            return 'Authenticator';
+        case 'backup-code':
+            return 'Backup Codes';
+        case 'magic-link':
+            return 'Magic Link';
+        default:
+            return 'Unknown';
+        }
+    }
 });
